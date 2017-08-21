befe1ab24b1d6fb0ab7f02cb3b810d740038d50e
Eduardo Aparicio Cardenes <eduardo.aparicio.cardenes@gmail.com>
2017-08-21 18:33:50 +0200

    Inject guid into contact

--- src/actions/contacts.js
+++ src/actions/contacts.js
@@ -1,8 +1,9 @@
 import { ADD_CONTACT } from '../constants/ActionTypes';
+import guid from '../utils/guid';
 
 export const addContact = (contact) => {
   return ({
     type: ADD_CONTACT,
-    contact
+    contact: Object.assign({}, contact, { id: guid() })
   });
 };
